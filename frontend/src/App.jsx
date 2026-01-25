ad46002658aae6922428bc58a7eae8de257a3271
xiaoze-xiaoze <hanze06@163.com>
2026-01-25 20:50:55 +0800

    update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,14 +3,14 @@ import './App.css'
 function App() {
   return (
     <div className="app">
-      <div className="title"></div>
-      <div className="historical"></div>
-      <div className="models"></div>
-      <div className="chat"></div>
-      <div className="input"></div>
-      <div className="settings"></div>
+      <div className="title">Viper</div>
+      <div className="models">Select Models</div>
+      <div className="historical">Historical</div>
+      <div className="chat">Chat</div>
+      <div className="input">Input</div>
+      <div className="settings">Settings</div>
     </div>
-  )
+  );
 }
 
 export default App;
