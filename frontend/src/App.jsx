41bbf144138465581c9b455fdac8104cc786906d
xiaoze-xiaoze <hanze06@163.com>
2026-01-25 15:45:43 +0800

    update

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="app">
+      <div className="title"></div>
+      <div className="historical"></div>
+      <div className="models"></div>
+      <div className="chat"></div>
+      <div className="input"></div>
+      <div className="settings"></div>
+    </div>
+  )
+}
+
+export default App;
